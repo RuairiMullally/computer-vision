1f5484f2680a992e4626bd2cc7cdc633796d5633
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2025-10-25 23:55:45 +0100

    Change to GMM, rough persistence.

--- MyApplication.cpp
+++ MyApplication.cpp
@@ -65,23 +65,42 @@ void MyApplication()
 			//store frame 3 of the video as the starting background frame
 			Mat background_frame = current_frame.clone();
 			// after: Mat background_frame = current_frame.clone();
-			cv::Mat consec_count(current_frame.rows, current_frame.cols, CV_16U, cv::Scalar(0));
-			cv::Mat persistentMask(current_frame.rows, current_frame.cols, CV_8U, cv::Scalar(0));
+			cv::Ptr<cv::BackgroundSubtractorMOG2> mog1 = cv::createBackgroundSubtractorMOG2();
+			mog1->setVarThreshold(25);
+			mog1->setDetectShadows(true);
+
+			cv::Ptr<cv::BackgroundSubtractorMOG2> mog2 = cv::createBackgroundSubtractorMOG2();
+			mog2->setVarThreshold(25);
+			mog2->setDetectShadows(true);
+
+			const int PERSIST_FRAMES = 30;
+			Mat persistCount(current_frame.rows, current_frame.cols, CV_16U, Scalar(0)); 
+			Mat persistentMask(current_frame.rows, current_frame.cols, CV_16U, Scalar(0));
 			// now loop through the video, comparing each frame with the background frame
 			while (!current_frame.empty())
 			{
 				//compute the absolute difference between the current frame and the background frame
-				Mat difference_frame;
-				absdiff(current_frame, background_frame, difference_frame);
-				//convert the difference frame to grayscale
-				cvtColor(difference_frame, difference_frame, COLOR_BGR2GRAY);
-				//threshold the difference image to get a binary image
-				threshold(difference_frame, difference_frame, 30, 255, THRESH_BINARY);
-				// perform opening and closing to remove noise and fill in holes
-				morphologyEx(difference_frame, difference_frame, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
-				morphologyEx(difference_frame, difference_frame, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(12, 12)));
+				Mat difference_frame, difference_frame2;
+				mog1->apply(current_frame, difference_frame, 0.004);
+				mog2->apply(current_frame, difference_frame2, 0.001);
+				Mat fgBinary = (difference_frame == 255);
+				Mat fgBinary2 = (difference_frame2 == 255);
+
+				morphologyEx(fgBinary, fgBinary, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
+				morphologyEx(fgBinary, fgBinary, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(12, 12)));
+				morphologyEx(fgBinary2, fgBinary2, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
+				morphologyEx(fgBinary2, fgBinary2, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(12, 12)));
 				// TODO: if a pixel has been white in the difference frame for 20 consecutive frames, draw a red mask in that position over the original image
-				
+				// find a persistent change by considering pixels where slow mask shows change but quick mask does not
+				Mat changeCandidate;
+				bitwise_and(fgBinary2, ~fgBinary, changeCandidate);
+				morphologyEx(fgBinary, fgBinary, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(5, 5))); // apply closing to a candidate
+
+				persistCount.setTo(0, changeCandidate == 0);  // reset where background
+				add(persistCount, Scalar(1), persistCount, changeCandidate); // increment where changed
+				compare(persistCount, Scalar(PERSIST_FRAMES), persistentMask, CMP_GE);
+
+
 
 				// Draw ground truth
 				for (int current = 0; (current < sizeof(object_locations) / 7); current++)
@@ -102,8 +121,14 @@ void MyApplication()
 				imshow(string("Original - ") + abandoned_removed_video_files[video_file_no-1], current_frame);
 
 				// Display the difference frame
-				writeText(difference_frame, frame_title, 15, 15);
-				imshow(string("Difference - ") + abandoned_removed_video_files[video_file_no-1], difference_frame);
+				writeText(fgBinary, frame_title, 15, 15);
+				imshow(string("Difference - ") + abandoned_removed_video_files[video_file_no-1], fgBinary);
+
+				writeText(fgBinary2, frame_title, 15, 15);
+				imshow(string("Difference2 - ") + abandoned_removed_video_files[video_file_no-1], fgBinary2);
+
+				writeText(persistentMask, frame_title, 15, 15);
+				imshow(string("persistentMask - ") + abandoned_removed_video_files[video_file_no-1], persistentMask);
 
 				char choice = cv::waitKey(50);  // Delay between frames
 				video[video_file_no-1] >> current_frame;
